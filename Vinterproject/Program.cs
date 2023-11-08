9a96cc4c21d6175d62a7b264a3fdfda8d699f945
Axel Grufman <Axel.Grufman@elev.ga.ntig.se>
2023-11-08 15:41:41 +0100

    First part done

--- Vinterproject/Program.cs
+++ Vinterproject/Program.cs
@@ -3,9 +3,9 @@ int vatten = 100;
 int dag = 0;
 int time = 12;
 
-String nuvaranderum = "Flygner";
+String nuvaranderum = "Flygner1";
 
-
+flygner1();
 
 
 
@@ -17,15 +17,24 @@ void flygner1()
     Console.WriteLine("Du var på väg till philipinerna när dit plan började störta");
     Console.WriteLine("Du antar att planet krashade och du blev medvetslös");
 
-  Console.WriteLine("Du kollar runt dig men ser att alla andra är döda");
     Console.WriteLine("");
+    Console.WriteLine("Du kollar runt dig men ser att alla andra är döda");
+    Console.WriteLine("Du ställer dig upp i stolen och ser att planet är i två delar");
+    Console.WriteLine("Du tar dig ur flygplanet och ser att du är i en jungel");
+    Console.WriteLine("Du ser en väg genom träden och väljer att följa den");
 
 
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("Tryck på space för att fortsätta");
     Console.ReadLine();
 }
-void Flygner2()
-{
-    Console.WriteLine("");
+// void Flygner2()
+// {
+//     Console.WriteLine("");
 
 
 
@@ -36,126 +45,126 @@ void Flygner2()
 
 
 
-    Console.ReadLine();
-}
-void Litenflod()
-{
-    Console.ReadLine();
-}
-void hem()
-{
-    Console.ReadLine();
-}
-void Storöppnigne()
-{
-    Console.ReadLine();
-}
-void Helikopterner()
-{
-    Console.ReadLine();
-}
-void orm()
-{
-    Console.ReadLine();
-}
-void storflodkant()
-{
-    Console.ReadLine();
-}
-void evigvägupp()
-{
-    Console.ReadLine();
-}
-void evigvägner()
-{
-    Console.ReadLine();
-}
-void båt()
-{
-    Console.ReadLine();
+//     Console.ReadLine();
+// }
+// void Litenflod()
+// {
+//     Console.ReadLine();
+// }
+// void hem()
+// {
+//     Console.ReadLine();
+// }
+// void Storöppnigne()
+// {
+//     Console.ReadLine();
+// }
+// void Helikopterner()
+// {
+//     Console.ReadLine();
+// }
+// void orm()
+// {
+//     Console.ReadLine();
+// }
+// void storflodkant()
+// {
+//     Console.ReadLine();
+// }
+// void evigvägupp()
+// {
+//     Console.ReadLine();
+// }
+// void evigvägner()
+// {
+//     Console.ReadLine();
+// }
+// void båt()
+// {
+//     Console.ReadLine();
 
-}
-void corridor1()
-{
-    Console.ReadLine();
-}
-void corridor2()
-{
-    Console.ReadLine();
-}
-void corridor3()
-{
-    Console.ReadLine();
-}
-void corridor4()
-{
-    Console.ReadLine();
-}
-void corridor5()
-{
-    Console.ReadLine();
-}
-void corridor6()
-{
-    Console.ReadLine();
-}
-void corridor7()
-{
-    Console.ReadLine();
-}
-void corridor8()
-{
-    Console.ReadLine();
-}
-void corridor9()
-{
-    Console.ReadLine();
-}
-void corridor10()
-{
-    Console.ReadLine();
-}
-void corridor11()
-{
-    Console.ReadLine();
-}
-void corridor12()
-{
-    Console.ReadLine();
-}
-void corridor13()
-{
-    Console.ReadLine();
-}
-void corridor14()
-{
-    Console.ReadLine();
-}
-void corridor15()
-{
-    Console.ReadLine();
-}
-void corridor16()
-{
-    Console.ReadLine();
-}
-void corridor17()
-{
-    Console.ReadLine();
-}
-void corridor18()
-{
-    Console.ReadLine();
-}
-void corridor19()
-{
-    Console.ReadLine();
-}
-void corridor20()
-{
-    Console.ReadLine();
-}
-void corridor21()
+// }
+// void corridor1()
+// {
+//     Console.ReadLine();
+// }
+// void corridor2()
+// {
+//     Console.ReadLine();
+// }
+// void corridor3()
+// {
+//     Console.ReadLine();
+// }
+// void corridor4()
+// {
+//     Console.ReadLine();
+// }
+// void corridor5()
+// {
+//     Console.ReadLine();
+// }
+// void corridor6()
+// {
+//     Console.ReadLine();
+// }
+// void corridor7()
+// {
+//     Console.ReadLine();
+// }
+// void corridor8()
+// {
+//     Console.ReadLine();
+// }
+// void corridor9()
+// {
+//     Console.ReadLine();
+// }
+// void corridor10()
+// {
+//     Console.ReadLine();
+// }
+// void corridor11()
+// {
+//     Console.ReadLine();
+// }
+// void corridor12()
+// {
+//     Console.ReadLine();
+// }
+// void corridor13()
+// {
+//     Console.ReadLine();
+// }
+// void corridor14()
+// {
+//     Console.ReadLine();
+// }
+// void corridor15()
+// {
+//     Console.ReadLine();
+// }
+// void corridor16()
+// {
+//     Console.ReadLine();
+// }
+// void corridor17()
+// {
+//     Console.ReadLine();
+// }
+// void corridor18()
+// {
+//     Console.ReadLine();
+// }
+// void corridor19()
+// {
+//     Console.ReadLine();
+// }
+// void corridor20()
+// {
+//     Console.ReadLine();
+// }
+// void corridor21()
 {
     Console.ReadLine();
 }
