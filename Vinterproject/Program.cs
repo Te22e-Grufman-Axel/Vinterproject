6b253611f74b12d67c26aa4ab5f2c8cf928866ce
Axel Grufman <Axel.Grufman@elev.ga.ntig.se>
2023-11-08 11:02:31 +0100

    Börjat projecket

--- Vinterproject/Program.cs
+++ Vinterproject/Program.cs
@@ -1,1 +1,161 @@
-﻿
+﻿int Mat = 100;
+int vatten = 100;
+int dag = 0;
+int time = 12;
+
+String nuvaranderum = "Flygner";
+
+
+
+
+
+void flygner1()
+{
+    Console.WriteLine("Du vaknar upp i din stol");
+    Console.WriteLine("Du undrar var du är, vad hände");
+    Console.WriteLine("Du börjar komma ihåg");
+    Console.WriteLine("Du var på väg till philipinerna när dit plan började störta");
+    Console.WriteLine("Du antar att planet krashade och du blev medvetslös");
+
+  Console.WriteLine("Du kollar runt dig men ser att alla andra är döda");
+    Console.WriteLine("");
+
+
+    Console.ReadLine();
+}
+void Flygner2()
+{
+    Console.WriteLine("");
+
+
+
+
+
+
+
+
+
+
+    Console.ReadLine();
+}
+void Litenflod()
+{
+    Console.ReadLine();
+}
+void hem()
+{
+    Console.ReadLine();
+}
+void Storöppnigne()
+{
+    Console.ReadLine();
+}
+void Helikopterner()
+{
+    Console.ReadLine();
+}
+void orm()
+{
+    Console.ReadLine();
+}
+void storflodkant()
+{
+    Console.ReadLine();
+}
+void evigvägupp()
+{
+    Console.ReadLine();
+}
+void evigvägner()
+{
+    Console.ReadLine();
+}
+void båt()
+{
+    Console.ReadLine();
+
+}
+void corridor1()
+{
+    Console.ReadLine();
+}
+void corridor2()
+{
+    Console.ReadLine();
+}
+void corridor3()
+{
+    Console.ReadLine();
+}
+void corridor4()
+{
+    Console.ReadLine();
+}
+void corridor5()
+{
+    Console.ReadLine();
+}
+void corridor6()
+{
+    Console.ReadLine();
+}
+void corridor7()
+{
+    Console.ReadLine();
+}
+void corridor8()
+{
+    Console.ReadLine();
+}
+void corridor9()
+{
+    Console.ReadLine();
+}
+void corridor10()
+{
+    Console.ReadLine();
+}
+void corridor11()
+{
+    Console.ReadLine();
+}
+void corridor12()
+{
+    Console.ReadLine();
+}
+void corridor13()
+{
+    Console.ReadLine();
+}
+void corridor14()
+{
+    Console.ReadLine();
+}
+void corridor15()
+{
+    Console.ReadLine();
+}
+void corridor16()
+{
+    Console.ReadLine();
+}
+void corridor17()
+{
+    Console.ReadLine();
+}
+void corridor18()
+{
+    Console.ReadLine();
+}
+void corridor19()
+{
+    Console.ReadLine();
+}
+void corridor20()
+{
+    Console.ReadLine();
+}
+void corridor21()
+{
+    Console.ReadLine();
+}
